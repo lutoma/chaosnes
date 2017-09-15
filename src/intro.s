f729fa247f79fa4b18554b011e37470c49563aeb
Lukas Martini <hello@lutoma.org>
2017-09-15 02:43:42 +0200

    More fancy intro logos

--- src/intro.s
+++ src/intro.s
@@ -19,14 +19,30 @@ intro_palette:
 	pal 		$00, $10, $20	; Grey; light grey; white.
 	pal 		$25, $37, $27	; Pink; light yellow; orange.
 .endrepeat
+rzl_logo_1:
+	.byt 128,129,130,131,132,0
+rzl_logo_2:
+	.byt 133,134,135,136,137,0
+rzl_logo_3:
+	.byt 138,139,140,141,142,0
+rzl_logo_4:
+	.byt 143,144,145,146,147,0
+rzl_logo_5:
+	.byt 148,149,150,0
 
 rzl_presents:
 	    ; 01234567890123456789012345678901
 	.byt "  RaumZeitLabor Entertainment   "
 	.byt "            presents            "
 	.byt "                                "
-	.byt "    code by lutoma & silsha     "
-	.byt "       graphics by silsha       ",0
+	.byt "   crafted by lutoma & silsha   ",0
+
+main_logo_1:
+	.byt 160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,0
+main_logo_2:
+	.byt 176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,0
+main_logo_3:
+	.byt 192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,0
 
 .code
 .export intro
@@ -37,7 +53,15 @@ intro:
 	lda #0				; First song
 	jsr pently_start_music
 
-	write_text rzl_presents, $20, (32*2)
+	write_text rzl_logo_1, $20, (32*3 + 13)
+	write_text rzl_logo_2, $20, (32*4 + 13)
+	write_text rzl_logo_3, $20, (32*5 + 13)
+	write_text rzl_logo_4, $20, (32*6 + 13)
+	write_text rzl_logo_5, $20, (32*7 + 13)
+	write_text rzl_presents, $21, (32*3)
+	write_text main_logo_1, $22, (32*3 + 8)
+	write_text main_logo_2, $22, (32*4 + 8)
+	write_text main_logo_3, $22, (32*5 + 8)
 
 message_done:
 	; Wait for a while
